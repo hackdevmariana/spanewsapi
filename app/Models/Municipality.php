5c7ba140441d64e52b5234b3f67aa066a3b4876a
Fernando <fernando@juandemariana.org>
2025-06-22 18:12:11 +0200

    Add Municipality model relations.

--- app/Models/Municipality.php
+++ app/Models/Municipality.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Municipality extends Model
 {
-    //
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function sources()
+    {
+        return $this->hasMany(Source::class);
+    }
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
