3347009380edd459776769ec1ef63035f779d77a
Fernando <fernando@juandemariana.org>
2025-06-22 18:10:41 +0200

    Add Article model relations.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+
+    public function municipality()
+    {
+        return $this->belongsTo(Municipality::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function featured()
+    {
+        return $this->hasOne(FeaturedArticle::class);
+    }
 }
