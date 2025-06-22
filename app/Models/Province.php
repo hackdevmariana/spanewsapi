8418bf8704bf1c55761bbcda682cba0c00569be7
Fernando <fernando@juandemariana.org>
2025-06-22 18:25:36 +0200

    Add Province Filament resource.

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Province extends Model
 {
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
     public function municipalities()
     {
         return $this->hasMany(Municipality::class);
