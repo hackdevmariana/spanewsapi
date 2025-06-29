cd12ec8e7bb49d6268d21bd2b103a0754ab6087e
Fernando <fernando@juandemariana.org>
2025-06-29 15:48:25 +0200

    Automatically creates the slug from the name in the Tag model.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+
 
 class Tag extends Model
 {
@@ -11,6 +14,16 @@ class Tag extends Model
         'slug',
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($tag) {
+            $tag->slug = Str::slug($tag->name);
+        });
+
+        static::updating(function ($tag) {
+            $tag->slug = Str::slug($tag->name);
+        });
+    }
 
     public function articles()
     {
