515d586e5970ee8cf9c4503aa0fe002a8836b1e0
Fernando <fernando@juandemariana.org>
2025-06-29 15:53:51 +0200

    Add slug to Topic model.

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -3,9 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Topic extends Model
 {
+    protected $fillable = ['name', 'slug'];
+
+    public static function booted()
+    {
+        static::creating(function ($topic) {
+            $topic->slug = Str::slug($topic->name);
+        });
+
+        static::updating(function ($topic) {
+            $topic->slug = Str::slug($topic->name);
+        });
+    }
+
     public function sources()
     {
         return $this->hasMany(Source::class);
